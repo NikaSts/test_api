34754ffbbd698d7b5a6352fa4416ff59d9f769cb
Nefila <veronika.sts@gmail.com>
2021-11-28 12:09:08 +0100

    Added logger

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express, { Express } from "express";
 import { Server } from 'http';
-import { userRouter } from './users/users.js';
+import { LoggerService } from "./logger/logger.service";
+import { userRouter } from './users/users';
 
 export class App {
 	app: Express;
 	server: Server;
 	port: number;
+	logger: LoggerService;
 
-	constructor() {
+	constructor(logger: LoggerService) {
 		this.app = express();
 		this.port = 8000;
+		this.logger = new LoggerService();
 	}
 
 	useRoutes() {
@@ -21,6 +24,6 @@ export class App {
 		this.useRoutes();
 		this.server = this.app.listen(this.port);
 
-		console.log(`Server on http://localhost:${this.port}`);
+		this.logger.log(`Server on http://localhost:${this.port}`);
 	}
 }
