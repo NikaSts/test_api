12c0acfcd5d8fbfc6d5aeeaf0f2d6be74c2a8cc4
Nefila <veronika.sts@gmail.com>
2022-01-27 17:26:54 +0100

    Added authorization

--- src/users/users.controller.interface.ts
+++ src/users/users.controller.interface.ts
@@ -4,4 +4,5 @@ import { IBaseController } from '../common/base.controller.interface';
 export interface IUserController extends IBaseController {
 	login: (req: Request, res: Response, next: NextFunction) => void;
 	register: (req: Request, res: Response, next: NextFunction) => void;
+	info: (req: Request, res: Response, next: NextFunction) => void;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,23 @@
 import { Response, Request, NextFunction } from 'express';
 import { inject, injectable } from 'inversify';
 import 'reflect-metadata';
+import { sign } from 'jsonwebtoken';
 import { BaseController } from '../common/base.controller';
 import { HTTPError } from '../errrors/http-error.class';
 import { ILogger } from '../logger/logger.interface';
 import { IUserController } from './users.controller.interface';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
-import { UserService } from './users.service';
+import { IUserService } from './users.service.interface';
 import { TYPES } from '../types';
 import { ValidateMiddleware } from '../common/validate.middleware';
+import { IConfigService } from '../config/config.interface';
+import { AuthGuard } from '../common/auth.guard';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
-	@inject(TYPES.UserService) private userService: UserService;
+	@inject(TYPES.UserService) private userService: IUserService;
+	@inject(TYPES.ConfigService) private configService: IConfigService;
 
 	constructor(logger: ILogger) {
 		super(logger);
@@ -29,6 +33,12 @@ export class UserController extends BaseController implements IUserController {
 				method: 'post',
 				func: this.login,
 				middlewares: [new ValidateMiddleware(UserLoginDto)],
+			},
+			{
+				path: '/info',
+				method: 'get',
+				func: this.info,
+				middlewares: [new AuthGuard()],
 			},
 		]);
 	}
@@ -54,6 +64,36 @@ export class UserController extends BaseController implements IUserController {
 		if (!isExist) {
 			return next(new HTTPError(401, `Authorization error`));
 		}
-		this.ok(res, { loginSuccess: isExist });
+
+		const secret = this.configService.get<string>('SECRET');
+		const jwt = await this.signJWT(req.body.email, secret);
+		this.ok(res, { loginSuccess: isExist, token: jwt });
+	}
+
+	async info(req: Request, res: Response, next: NextFunction): Promise<void> {
+		const userInfo = await this.userService.getUserInfo(req.user);
+		this.ok(res, { email: userInfo?.email, id: userInfo?.id });
+	}
+
+	private signJWT(email: string, secret: string): Promise<string> {
+		return new Promise((resolve, reject) => {
+			sign(
+				{
+					email,
+					iat: Math.floor(Date.now() / 1000),
+				},
+				secret,
+				{
+					algorithm: 'HS256',
+				},
+				(err, token) => {
+					if (err) {
+						reject(err);
+					} else {
+						resolve(token as string);
+					}
+				},
+			);
+		});
 	}
 }
